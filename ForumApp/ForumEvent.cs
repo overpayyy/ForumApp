8c9ece582965a482d0c3dc068acd1c7df04d4399
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-31 14:23:05 +0200

    ForumEvent.cs

--- ForumApp/ForumEvent.cs
+++ ForumApp/ForumEvent.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ForumApp
+﻿namespace ForumApp
 {
-    internal class ForumEvent
+    public class ForumEvent
     {
+        public ForumEventType EventType { get; }
+        public string UserLogin { get; }
+        public string QuestionId { get; }
+        public string AnswerId { get; }
+        public string QuestionAuthorLogin { get; }
+
+        public ForumEvent(ForumEventType eventType, string userLogin, string questionId, string answerId = null, string questionAuthorLogin = null)
+        {
+            EventType = eventType;
+            UserLogin = userLogin;
+            QuestionId = questionId;
+            AnswerId = answerId;
+            QuestionAuthorLogin = questionAuthorLogin;
+        }
     }
 }
