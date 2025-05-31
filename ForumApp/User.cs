04d213bbc52fc7230a8f069384504a605c4c1502
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-31 14:52:34 +0200

    User.cs

--- ForumApp/User.cs
+++ ForumApp/User.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ForumApp
+﻿namespace ForumApp
 {
-    internal class User
+    public class User
     {
+        public string Login { get; }
+        public bool SubscribedToAll { get; private set; }
+        public bool SubscribedToOwnQuestions { get; private set; }
+
+        public User(string login)
+        {
+            Login = login;
+        }
+
+        public void SubscribeToAll()
+        {
+            SubscribedToAll = true;
+        }
+
+        public void UnsubscribeFromAll()
+        {
+            SubscribedToAll = false;
+        }
+
+        public void SubscribeToOwnQuestions()
+        {
+            SubscribedToOwnQuestions = true;
+        }
+
+        public void UnsubscribeFromOwnQuestions()
+        {
+            SubscribedToOwnQuestions = false;
+        }
+
+        public void HandleForumEvent(ForumEvent forumEvent)
+        {
+            if (forumEvent.EventType == ForumEventType.QuestionAdded)
+            {
+                if (SubscribedToAll)
+                {
+                    Console.WriteLine($"User {Login} notified: User {forumEvent.UserLogin} added question {forumEvent.QuestionId}");
+                }
+            }
+            else if (forumEvent.EventType == ForumEventType.AnswerAdded)
+            {
+                if (SubscribedToAll)
+                {
+                    Console.WriteLine($"User {Login} notified: User {forumEvent.UserLogin} added answer {forumEvent.AnswerId} to question {forumEvent.QuestionId} by {forumEvent.QuestionAuthorLogin}");
+                }
+                if (SubscribedToOwnQuestions && Login == forumEvent.QuestionAuthorLogin)
+                {
+                    Console.WriteLine($"User {Login} notified: User {forumEvent.UserLogin} added answer {forumEvent.AnswerId} to your question {forumEvent.QuestionId}");
+                }
+            }
+        }
     }
 }
